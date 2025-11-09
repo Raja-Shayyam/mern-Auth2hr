de05a67ac0631e4a8c745b0a2acf869ba23532af
Rehan Raja <rehman0rehan@gmail.com>
2025-11-09 16:21:52 +0500

    Update Server.js

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -10,7 +10,8 @@ const myPort = process.env.PORT
 const App = express()
 
 App.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'http://localhost:5173', // Your local frontend URL
+  origin: 'https://mern-auth2hr-wvzs-2gv738ksn-rehman0rehan-gmailcoms-projects.vercel.app', // Your frontend URL
   credentials: true // Allow cookies to be sent
 }))
 

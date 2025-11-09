34d8beebc7b07cce0ddcd1c15415bfeff272364a
Rehan Raja <rehman0rehan@gmail.com>
2025-11-09 20:50:50 +0500

    Update mongooseDB.js

--- BackEnd/mongooseDB/mongooseDB.js
+++ BackEnd/mongooseDB/mongooseDB.js
@@ -13,14 +13,14 @@ export const conectOdb = async () => {
 
   try {
     mongoose.set('strictQuery', false)
-    await mongoose.connect(process.env.MONGODB_COMPAS, {
-      bufferCommands: false, // Disable buffering for serverless
-      maxPoolSize: 10, // Limit connection pool
-    })
-    // await mongoose.connect(process.env.MONGO_ATLS_2, {
+    // await mongoose.connect(process.env.MONGODB_COMPAS, {
     //   bufferCommands: false, // Disable buffering for serverless
     //   maxPoolSize: 10, // Limit connection pool
     // })
+    await mongoose.connect(process.env.MONGO_ATLS_2, {
+      bufferCommands: false, // Disable buffering for serverless
+      maxPoolSize: 10, // Limit connection pool
+    })
     console.log('connection established');
     isConected = true;
   } catch (error) {
